b9a604e7842e704823a060b8f7fafbd73bb7b630
Karthikeyan Duriavel <75940774+KoderDurai@users.noreply.github.com>
2024-05-17 21:03:37 +0100

    Updates

---
+++ WarGameY/WarGameYApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarGameYApp.swift
+//  WarGameY
+//
+//  Created by Karthikeyan Duraivel on 17/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WarGameYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
